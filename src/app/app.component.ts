022e52c8bf1ad00d6a1b0078645fbebdef1e0931
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-03-08 09:46:55 +0100

    Add hero editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Bienvenue sur le site de Wendy !';
+  title = 'Tour of Heroes.';
 }
